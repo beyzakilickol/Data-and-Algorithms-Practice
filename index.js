a4b842ee431356a14b54a298674d766d286e6fd4
beyzakilickol <43250495+beyzakilickol@users.noreply.github.com>
2018-12-09 19:26:36 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,108 @@
+//find the element which does not fit the pattern(even,odd)
+var arr = [1,1,3,5,8,7]
+var binarySearch = []
+
+  for(var i = 0; i< arr.length; i++){
+    if(arr[i] % 2 == 0){
+      binarySearch.push(0)
+  } else{
+      binarySearch.push(1)
+  }
+}
+
+var result = ''
+ let sortedArr = binarySearch.sort(function(a, b){return a - b})
+ if(sortedArr[1] == 0){
+    result = "odd"
+ } else {
+  result = "even"
+ }
+ for(var i = 0; i< arr.length; i++){
+   if(result == "odd" && arr[i] % 2 != 0){
+    console.log(arr.indexOf(arr[i]) + 1)
+  }
+  else if(result == "even" && arr[i] % 2 == 0){
+    console.log(arr.indexOf(arr[i]) + 1)
+ }
+ }
+ //-------------------String to camelcase and combined------------------------------------
+let str = "The-stealth-warrior" //Output: TheStealthWarrior
+let capitalizedArr = []
+ function toCamelCase(str){
+ if(str.includes('-')==true){
+   console.log(str.split('-'))
+ let arr = str.split('-')
+ capitalizedArr.push(arr[0])
+ for(var i=1;i<arr.length;i++){
+   capitalizedArr.push(arr[i].charAt(0).toUpperCase() + arr[i].slice(1))
+ }
+
+ console.log(capitalizedArr.join(''))
+} else if(str.includes('_')==true){
+let arr = str.split('_')
+capitalizedArr.push(arr[0])
+for(var i=1;i<arr.length;i++){
+  capitalizedArr.push(arr[i].charAt(0).toUpperCase() + arr[i].slice(1))
+}
+
+console.log(capitalizedArr.join(''))
+}
+}
+toCamelCase(str)
+//-------------------Equal sum of each side of an index-----------------------------------------------------
+let array = [1,2,3,4,3,2,1] //Outcome: 3.index and number is 4.
+let result1;
+function findEvenIndex(array){
+  for(var i=0;i<array.length;i++){
+    let leftarray = array.slice(0,i)
+    let rigtharray = array.slice(i+1)
+    let leftSum= leftarray.reduce((a,b)=> a+b,0)
+    let rigthSum= rigtharray.reduce((a,b)=> a+b,0)
+    if(leftSum==rigthSum){
+       result1 = i
+    }
+  }
+  return result1
+}
+findEvenIndex(array)
+//-----------------------Remove smallest number in an array, get smallest index if there are more than 1---------------------------------------
+let arry = [6,2,3,2,4,5]
+
+let min = Math.min(...arry)
+let index=[1]
+
+for(var i = 0; i<arry.length;i++){
+  if(arry[i]==min && index.length==0){
+    index.push(arry.indexOf(arry[i]))
+  }
+}
+let newArry = arry.slice(0,index[0]).concat(arry.slice(index[0]+1))
+console.log(newArry)
+//-----------------------------Palindrome----------------------------
+let string = "emre"
+function isPalindrome(string){
+let stringarr = string.toLowerCase().split('')
+let reversedstringarr = stringarr.reverse()
+let reversedstring=reversedstringarr.join('')
+if(string == reversedstring){
+  return true
+} else{
+  return false
+}
+}
+console.log(isPalindrome(string))
+//--------------------------------FizzBuzz---------------------------
+function isFizzBuzz(number){
+  if(number % 15 ==0){
+    return 'fizzbuzz'
+  }else if(number % 5 ==0){
+    return 'fizz'
+  } else if(number % 3 == 0){
+    return 'buzz'
+  } else {
+    return 'neither'
+  }
+
+}
+console.log(isFizzBuzz(32))
+//-------------------------------------------------------
