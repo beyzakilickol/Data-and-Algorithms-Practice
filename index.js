8561b370509c8067077dbfcc34a1a6a98717a45a
beyzakilickol <43250495+beyzakilickol@users.noreply.github.com>
2019-01-27 15:36:21 -0600

    practice

--- index.js
+++ index.js
@@ -457,3 +457,19 @@ public int array667(int[] nums) {
 var breakChocolate = function(n, m) {
  return (n*m === 0) ? 0 : n * m - 1;
 };
+
+// --------------------- string Splosion ----------------
+// Given a non-empty string like "Code" return a string like "CCoCodCode".
+//
+// stringSplosion("Code") → "CCoCodCode"
+// stringSplosion("abc") → "aababc"
+// stringSplosion("ab") → "aab"
+
+public String stringSplosion(String str) {
+  String result = "";
+  // On each iteration, add the substring of the chars 0..i
+  for (int i=0; i<str.length(); i++) {
+    result = result + str.substring(0, i+1);
+  }
+  return result;
+}
