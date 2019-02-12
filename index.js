c937576627c3083922a1abc94735e90c2b64bce9
beyzakilickol <43250495+beyzakilickol@users.noreply.github.com>
2019-02-12 15:52:21 -0600

    practicing

--- index.js
+++ index.js
@@ -407,3 +407,17 @@ function factorial(n) {
     throw new RangeError();
   return n <= 1 ? 1 : n * factorial(n - 1);
 }
+function first_non_repeating_letter(str){
+  let strArr = str.toLowerCase().split('')
+  console.log(strArr)
+  
+  for(let i=0; i<strArr.length; i++){
+    console.log(strArr.slice(0,i))
+    console.log(strArr.slice(i+1))
+    if(strArr.slice(i+1).includes(strArr[i]) == false && strArr.slice(0,i).includes(strArr[i])==false){
+       return strArr[i]
+    } 
+  }
+  return ''
+}
+console.log(first_non_repeating_letter('Hellohej'))
